ac529030064429993aca587cafea04148a8f939b
LeBadman <benny_badman@outlook.fr>
2017-06-08 02:18:07 +0200

    Init
    feat: init rep

---
+++ GoodCitizen.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         GoodCitizen
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Pour ne plus oublier son devoir de citoyen
+// @author       LeBadman
+// @match        https://www.youtube.com/watch*
+// @icon         http://image.noelshack.com/fichiers/2017/23/1496790392-goodcitizen.png
+// ==/UserScript==
+
+var userInfo;
+var sentiment;
+var userHeader;
+setInterval(actionCitoyenne, 100);
+
+function actionCitoyenne() {
+    if((userHeader=document.getElementById("watch7-user-header"))){
+        userInfo = userHeader.firstElementChild.nextElementSibling;
+        channelLink = userInfo.firstElementChild.getAttribute("href");
+        if(channelLink == "/channel/UCvmbmTfDzHmC6T6H-5evP1g"){
+            sentiment = document.getElementById("watch8-sentiment-actions");
+            span = sentiment.firstElementChild;
+            span = span.nextElementSibling;
+            span = span.lastElementChild;
+            span = span.previousElementSibling;
+            button = span.firstElementChild;
+            if(button.className.search("hid")==-1)
+                button.click();
+        }
+    }
+}
